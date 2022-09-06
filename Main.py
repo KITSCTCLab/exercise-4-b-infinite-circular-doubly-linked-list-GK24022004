104d4b1c4d9e4d5410985c7fd4f1519f09f7ec7a
GK24022004 <103179663+GK24022004@users.noreply.github.com>
2022-09-06 11:32:35 +0530

    EX 4

--- Main.py
+++ Main.py
@@ -1,5 +1,84 @@
-# Read an integer that denotes the length of the list which is returned as the output of the algorithm
-length_of_circular_linked_list = int(input())
-# Read space-separated integers that denote the elements of the list which is returned as the output of the algorithm
-circular_linked_list = list(map(int,input().strip().split(" ")))
-# Write your code here
+class Node:
+  def __init__(self,data):
+    self.data = data
+    self.next = None
+
+class Link:
+  def __init__(self):
+    self.head = None
+    self.count = 0
+  
+  def push(self,data):
+    new = Node(data)
+    new.next = self.head
+    self.head = new
+    self.count += 1
+  
+  def search(self,target):
+    n = self.head
+    result = 0
+    node_number = 0
+    new = {}
+    for i in range(self.count):
+      if(n.data == target):
+        new = {
+            "postion:":i,
+            "node value:":target
+        }
+    print()
+    return new
+
+        
+
+  def pair(self):
+    n = self.head
+    while(n):
+      print(n.data,end = "->")
+      n = n.next
+    print("\n")
+    
+  def delete_beg(self):
+    temp = self.head
+    self.head = self.head.next
+    temp = None
+  
+  def delete_end(self):
+    for i in range(self.count):
+      if(i == self.count - 1):
+        temp = self.head
+        self.head.next = None
+        temp = None
+
+
+  def reverse(self):
+    curr , prev , next = self.head , None,None
+    while(curr != None):
+      next = curr.next
+      curr.next = prev
+      prev = curr
+      curr = next
+    self.head = prev
+
+l1 = Link()
+# while(1):
+#   x = int(input())
+#   if(x == 1):
+#     val = int(input())
+#     l1.push(val)
+#   elif(x == 2):
+#     l1.reverse()
+#   elif(x == 3):
+#     l1.pair()
+#   elif(x == 4):
+#     search = int(input())
+#     print(l1.search(search))
+
+for i in range(3):
+  val = int(input())
+  l1.push(val)
+
+l1.pair()
+l1.delete_beg()
+l1.pair()
+l1.delete_end()
+l1.pair()
