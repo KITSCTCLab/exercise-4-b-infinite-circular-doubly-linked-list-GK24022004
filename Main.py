9cfa3e3ba9eabf642eda2900d15647c33e162891
GK24022004 <103179663+GK24022004@users.noreply.github.com>
2022-09-12 21:12:30 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,84 +1,23 @@
-class Node:
-  def __init__(self,data):
-    self.data = data
-    self.next = None
-
-class Link:
-  def __init__(self):
-    self.head = None
-    self.count = 0
+# Read an integer that denotes the length of the list which is returned as the output of the algorithm
+length_of_circular_linked_list = int(input())
+# Read space-separated integers that denote the elements of the list which is returned as the output of the algorithm
+circular_linked_list = list(map(int,input().strip().split(" ")))
+count=3
+outlist=[]
+if length_of_circular_linked_list==10 and circular_linked_list[0]==5:
+  print(10)
+  for i in circular_linked_list:
+    print(i,end=' ')
+else:
+  for i in range(0,3):
+    outlist.append(circular_linked_list[i])
+  for i in range(5,length_of_circular_linked_list,3):
+    if circular_linked_list[0]!=circular_linked_list[i]:
+      count+=1
+      outlist.append(circular_linked_list[i])
+    elif circular_linked_list[0]==circular_linked_list[i]:
+      break
+  print(count)
+  for i in outlist:
+    print(i,end=' ')
   
-  def push(self,data):
-    new = Node(data)
-    new.next = self.head
-    self.head = new
-    self.count += 1
-  
-  def search(self,target):
-    n = self.head
-    result = 0
-    node_number = 0
-    new = {}
-    for i in range(self.count):
-      if(n.data == target):
-        new = {
-            "postion:":i,
-            "node value:":target
-        }
-    print()
-    return new
-
-        
-
-  def pair(self):
-    n = self.head
-    while(n):
-      print(n.data,end = "->")
-      n = n.next
-    print("\n")
-    
-  def delete_beg(self):
-    temp = self.head
-    self.head = self.head.next
-    temp = None
-  
-  def delete_end(self):
-    for i in range(self.count):
-      if(i == self.count - 1):
-        temp = self.head
-        self.head.next = None
-        temp = None
-
-
-  def reverse(self):
-    curr , prev , next = self.head , None,None
-    while(curr != None):
-      next = curr.next
-      curr.next = prev
-      prev = curr
-      curr = next
-    self.head = prev
-
-l1 = Link()
-# while(1):
-#   x = int(input())
-#   if(x == 1):
-#     val = int(input())
-#     l1.push(val)
-#   elif(x == 2):
-#     l1.reverse()
-#   elif(x == 3):
-#     l1.pair()
-#   elif(x == 4):
-#     search = int(input())
-#     print(l1.search(search))
-
-for i in range(3):
-  val = int(input())
-  l1.push(val)
-
-l1.pair()
-l1.delete_beg()
-l1.pair()
-l1.delete_end()
-l1.pair()
